03f08142adaa41cf4ec915418f84cbd277c400d4
Shadow <thebotoftheshadow@gmail.com>
2021-11-09 11:40:32 -0600

    ope

--- commands/dev/eval.js
+++ commands/dev/eval.js
@@ -22,7 +22,7 @@ module.exports = {
 
             let out = await eval(code)()
             if (typeof out !== "string") out = require("util").inspect(out)
-            out = out.replace(process.env.TOKEN, "[TOKEN REDACTED]").replace(process.env.MONGODB, "[DB URI REDACTED]")
+            out = out.replace(process.env.TOKEN, "[TOKEN REDACTED]")
 
             message.channel.send(`Typeof output: **${typeof out}**`)
             message.channel.send({ content: out ? out : "null", split: true, code: "js" })
